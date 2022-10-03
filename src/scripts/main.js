4c2bff5738ffbdf35d6deed142f59e728fb0c09d
Sergio k <valodya.kirik@gmail.com>
2022-10-03 17:29:31 +0300

    added variables, a js file and added a bit of interactivity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,24 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__input');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
